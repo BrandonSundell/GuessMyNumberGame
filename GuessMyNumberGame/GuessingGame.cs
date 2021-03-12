51488f2ce32bba5c10d8164b681fdbe8f60526b9
Brandon Sundell <brandon.sundell@icloud.com>
2021-03-12 11:14:22 -0800

    Add project files.

---
+++ GuessMyNumberGame/GuessingGame.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessMyNumberGame
+{
+    class GuessingGame
+    {
+        public static Random rand = new Random();
+
+        public static void ComputerGuess()
+        {
+            Console.WriteLine("You're going to pick a number between 1 and 100! I'll try to guess what it is!");
+            Console.WriteLine("You'll let you know if my guess is to high or to low until I guess it!");
+            Console.WriteLine("Press Enter and I'll start guessing!");
+            Console.ReadLine();
+
+            bool right = false;
+            int min = 1;
+            int max = 100;
+            int comGuess = 0;
+            int timesGuessed = 0;
+            string userRespone = "";
+
+            while(right == false)
+            {
+                comGuess = rand.Next(min, max + 1);
+                Console.WriteLine($"Is your number {comGuess}? Yes[Y] To low[L] To high[H]");
+                userRespone = Console.ReadLine().ToLower();
+                while( userRespone != "y" && userRespone != "l" && userRespone != "h")
+                {
+                    Console.WriteLine("Error: Not a valid answer. Try again");
+                    userRespone = Console.ReadLine().ToLower();
+                }
+
+                if(userRespone == "y")
+                {
+                    right = true;
+                }
+                if(userRespone == "l")
+                {
+                    min = comGuess + 1;
+                }
+                if(userRespone == "h")
+                {
+                    max = comGuess - 1;
+                }
+                timesGuessed++;
+            }
+            Console.WriteLine($"I finaly guessed your number {comGuess}! It took me {timesGuessed} times to get it right.");
+
+        }
+        public static void PlayerGuess()
+        {
+            int comNum = rand.Next(1, 1001);
+            Console.WriteLine("I'm going to pick a number between 1 and 1000! Try to guess what it is!");
+            Console.WriteLine("I'll let you know if your guess is to high or to low until you guess it!");
+            Console.WriteLine("Start guessing!");
+            int userGuess = Int32.Parse(Console.ReadLine());
+
+            while(userGuess != comNum)
+            {
+                if(comNum > userGuess)
+                {
+                    Console.WriteLine("Your guess is to low. Try again.");
+                    userGuess = Int32.Parse(Console.ReadLine());
+                }
+                if(comNum < userGuess)
+                {
+                    Console.WriteLine("Your guess is to high. Try again.");
+                    userGuess = Int32.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Hey you got it right! Nice job! Thanks for playing!");
+        }
+    }
+}
---
+++ GuessMyNumberGame/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace GuessMyNumberGame
+{
+    class Program
+    {
+        public static int count = 0;
+        public static int BisectionSearch(int[] list, int userNum)
+
+        {
+            int middle = (list.Length) / 2;
+            int result = 0;
+            int[] narrowedList = new int[middle];
+
+
+                if (list.Length % 2 == 0)
+                {
+                    if (userNum == list[middle - 1])
+                    {
+                        Console.WriteLine($"Your number {userNum} was found in the list after {count} iterations.");
+                        return result;
+                    }
+                    else if (userNum > list[middle - 1])
+                    {
+                        Console.WriteLine($"Your number is greater than {middle}");
+                        //Console.WriteLine($"Your serch has been narrowed down to this list:");
+                        for (int i = middle; i < list.Length; i++)
+                        {
+                            narrowedList[i - middle] += list[i];
+                        }
+                        //PrintArray(narrowedList);
+                        //for (int i = middle + 1; i < list.Length; i++)
+                        //{
+                        //    narrowedList[i - middle - 1] = list[i];
+                        //}
+                    }
+                    else if (userNum < list[middle - 1])
+                    {
+                        Console.WriteLine($"Your number is less than {middle}");
+                        for (int i = 0; i < middle; i++)
+                        {
+                            narrowedList[i] += list[i];
+                        }
+                        //for (int i = 0; i < middle; i++)
+                        //{
+                        //    narrowedList[i] = list[i];
+                        //}
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your number could not be found in the list.");
+                    }
+                }
+                
+                if (list.Length % 2 == 1)
+                {
+                    if (userNum == list[middle])
+                    {
+                        Console.WriteLine($"Your number {userNum} was found in the list after {count} iterations.");
+                        return result;
+                    }
+                    else if (userNum > list[middle])
+                    {
+                        Console.WriteLine($"Your number is greater than {middle}");
+                        //Console.WriteLine($"Your serch has been narrowed down to this list:");
+                        for (int i = middle + 1; i < list.Length; i++)
+                        {
+                            narrowedList[i - middle - 1] += list[i];
+                        }
+                        //PrintArray(narrowedList);
+                    
+                        //for (int i = middle + 1; i < list.Length; i++)
+                        //{
+                        //    narrowedList[i - middle - 1] = list[i];
+                        //}
+                    }
+                    else if (userNum < list[middle])
+                    {
+                        Console.WriteLine($"Your number is less than {middle}");
+                        for (int i = 0; i < middle; i++)
+                        {
+                            narrowedList[i] += list[i];
+                        }
+                        //for (int i = 0; i < middle; i++)
+                        //{
+                        //    narrowedList[i] = list[i];
+                        //}
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your number could not be found in the list.");
+                    }
+                
+                } 
+                count++;
+                return BisectionSearch(narrowedList, userNum);
+        }
+        public static void PrintArray(int[] list)
+        {
+            foreach (int num in list)
+            {
+                Console.Write($"{num} ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            //int[] numList = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //PrintArray(numList);
+            //Console.WriteLine("\n Enter the number you need to find in the list");
+            //int userNum = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine(BisectionSearch(numList, userNum));
+            //GuessingGame.PlayerGuess();
+            GuessingGame.ComputerGuess();
+        }
+    }
+}
